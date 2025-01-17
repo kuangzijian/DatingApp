f4a8bd144c29c43ed25a9d4609a15726049272a6
Zijian Kuang <kuangzijian1@hotmail.com>
2025-01-17 16:51:35 -0700

    hook up register method

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -19,6 +19,19 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.curentUser.set(user);
         }
+        return user;
+      })
+    )
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.curentUser.set(user);
+        }
+        return user;
       })
     )
   }
