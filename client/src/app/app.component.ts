5b7a2f576a02c3fbbfb913a372b36e438b502447
Zijian Kuang <kuangzijian1@hotmail.com>
2025-01-21 14:26:35 -0700

    added routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,7 +13,7 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  private  accountService = inject(AccountService);
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
     this.SetCurrentUser();
