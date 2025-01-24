97784399eed421152d9cda88e5cc1b784456f094
Zijian Kuang <kuangzijian1@hotmail.com>
2025-01-23 21:12:34 -0700

    added dataseeding, repository pattern and auto mapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,29 +1,29 @@
-using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class UsersController(DataContext context) : BaseApiController
-{
-    private readonly DataContext _context = context;
-
-    [AllowAnonymous]
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
+{   
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await _context.Users.ToListAsync();
-        return users;
+        var users = await userRepository.GetMembersAsync();
+
+        return Ok(users);
     }
 
-    [Authorize]
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUser(int id)
+    [HttpGet("{username}")]
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await _context.Users.SingleOrDefaultAsync(x => x.Id == id);
+        var user = await userRepository.GetMemberAsync(username);
         if (user == null) return NotFound();
+
         return user;
     }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,6 +18,8 @@ public static class ApplicationServiceExtensions
         services.AddSwaggerGen();
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         return services;
     }
