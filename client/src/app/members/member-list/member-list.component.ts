92421316a82f78cf2552c44c09b0f2fc8d7f1b0b
Zijian Kuang <kuangzijian1@hotmail.com>
2025-01-30 21:41:32 -0700

    added member card and detail components

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
+import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent {
+  private memberService = inject(MembersService);
 
+  members: Member[] = [];
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe(
+      {
+        next: members => {
+          this.members = members
+        }
+    })
+  }
 }
