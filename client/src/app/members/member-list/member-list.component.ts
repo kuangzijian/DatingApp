ca05beb1e20177f0cd1aafd1b3dadd78b8ee1154
Zijian Kuang <kuangzijian1@hotmail.com>
2025-03-11 15:49:51 -0600

    added pagination feature

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,24 +1,35 @@
 import { Component, inject } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { MemberCardComponent } from "../member-card/member-card.component";
+import { PageChangedEvent, PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent],
+  imports: [MemberCardComponent, PaginationModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent {
+
   memberService = inject(MembersService);
+  pageNumber = 1;
+  pageSize = 5;
 
   ngOnInit(): void {
-    if (this.memberService.members().length === 0) {
+    if (!this.memberService.paginatedResult()) {
       this.loadMembers();
     }
   }
 
   loadMembers() {
-    this.memberService.getMembers()
+    this.memberService.getMembers(this.pageNumber, this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
