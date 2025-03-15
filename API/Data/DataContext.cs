a52856ab59b91c62928cff44b3e32d1fa2241110
Zijian Kuang <kuangzijian1@hotmail.com>
2025-03-15 14:54:04 -0600

    setup messages entities

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -6,9 +6,8 @@ namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public required DbSet<AppUser> Users { get; set; }
-
     public DbSet<UserLike> Likes { get; set; }
-
+    public DbSet<Message> Messages { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
@@ -27,5 +26,15 @@ public class DataContext(DbContextOptions options) : DbContext(options)
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.TargetUserId)
             .OnDelete(DeleteBehavior.NoAction);
+
+        builder.Entity<Message>()
+            .HasOne(m => m.Recipient)
+            .WithMany(m => m.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Message>()
+            .HasOne(m => m.Sender)
+            .WithMany(m => m.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
